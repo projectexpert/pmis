fe5f4c4e6bb14f347ba3134e910199f6c882ae40
Colin Newell <colin@opusvl.com>
2014-10-02 16:03:09 +0100

    Removed what I belive are redundant API elements. Made the code extend the sales orders in a simpler fashion.

--- project_sale/sale.py
+++ project_sale/sale.py
@@ -45,83 +45,12 @@ class sale_order_line(osv.osv):
         'order_project_manager': fields.related('order_project', 'user_id', readonly=True, string='Project Manager', type='many2one', relation="res.users", store=True),
     }
 
-    def invoice_line_create(self, cr, uid, ids, context=None):
-        if context is None:
-            context = {}
+    def _prepare_order_line_invoice_line(self, cr, uid, line, account_id=False, context=None):
+        res = super(sale_order_line, self)._prepare_order_line_invoice_line(cr, uid, line, account_id, context)
+        res['account_analytic_id'] = line.order_id.project and line.order_id.project.analytic_account_id \
+                                    and line.order_id.project.analytic_account_id.id or res['account_analytic_id']
+        return res
 
-        def _get_line_qty(line):
-            if (line.order_id.invoice_quantity=='order') or not line.procurement_id:
-                if line.product_uos:
-                    return line.product_uos_qty or 0.0
-                return line.product_uom_qty
-            else:
-                return self.pool.get('procurement.order').quantity_get(cr, uid,
-                        line.procurement_id.id, context=context)
-
-        def _get_line_uom(line):
-            if (line.order_id.invoice_quantity=='order') or not line.procurement_id:
-                if line.product_uos:
-                    return line.product_uos.id
-                return line.product_uom.id
-            else:
-                return self.pool.get('procurement.order').uom_get(cr, uid,
-                        line.procurement_id.id, context=context)
-
-        create_ids = []
-        sales = {}
-        for line in self.browse(cr, uid, ids, context=context):
-            if not line.invoiced:
-                if line.product_id:
-                    a = line.product_id.product_tmpl_id.property_account_income.id
-                    if not a:
-                        a = line.product_id.categ_id.property_account_income_categ.id
-                    if not a:
-                        raise osv.except_osv(_('Error !'),
-                                _('There is no income account defined ' \
-                                        'for this product: "%s" (id:%d)') % \
-                                        (line.product_id.name, line.product_id.id,))
-                else:
-                    prop = self.pool.get('ir.property').get(cr, uid,
-                            'property_account_income_categ', 'product.category',
-                            context=context)
-                    a = prop and prop.id or False
-                uosqty = _get_line_qty(line)
-                uos_id = _get_line_uom(line)
-                pu = 0.0
-                if uosqty:
-                    pu = round(line.price_unit * line.product_uom_qty / uosqty,
-                            self.pool.get('decimal.precision').precision_get(cr, uid, 'Sale Price'))
-                fpos = line.order_id.fiscal_position or False
-                a = self.pool.get('account.fiscal.position').map_account(cr, uid, fpos, a)
-                if not a:
-                    raise osv.except_osv(_('Error !'),
-                                _('There is no income category account defined in default Properties for Product Category or Fiscal Position is not defined !'))
-                inv_id = self.pool.get('account.invoice.line').create(cr, uid, {
-                    'name': line.name,
-                    'origin': line.order_id.name,
-                    'account_id': a,
-                    'price_unit': pu,
-                    'quantity': uosqty,
-                    'discount': line.discount,
-                    'uos_id': uos_id,
-                    'product_id': line.product_id.id or False,
-                    'invoice_line_tax_id': [(6, 0, [x.id for x in line.tax_id])],
-                    'note': line.notes,
-                    'account_analytic_id': line.order_id.project and line.order_id.project.analytic_account_id and line.order_id.project.analytic_account_id.id or False,
-                    
-                })
-                cr.execute('insert into sale_order_line_invoice_rel (order_line_id,invoice_id) values (%s,%s)', (line.id, inv_id))
-                self.write(cr, uid, [line.id], {'invoiced': True})
-                sales[line.order_id.id] = True
-                create_ids.append(inv_id)
-        # Trigger workflow events
-        wf_service = netsvc.LocalService("workflow")
-        for sid in sales.keys():
-            wf_service.trg_write(uid, 'sale.order', sid, cr)
-        return create_ids
-
-
-sale_order_line()
 
 class project(osv.osv):
     _inherit = "project.project"
@@ -147,5 +76,5 @@ class project(osv.osv):
 #                                                     }, help="Gives the number of sale order associated with the project"
                 ),
                 }
-project()
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
