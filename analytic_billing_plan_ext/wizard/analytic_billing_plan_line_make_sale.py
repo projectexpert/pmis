7041026fe9b723c546796d0a14216c8a0d912a6a
jbeficent <jordi.ballester@eficent.com>
2014-11-12 00:23:40 +0100

    Various corrections.

---
+++ analytic_billing_plan_ext/wizard/analytic_billing_plan_line_make_sale.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014 Eficent (<http://www.eficent.com/>)
+#               <contact@eficent.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from datetime import datetime
+from openerp.osv import fields, osv, orm
+from tools.translate import _
+
+
+class analytic_billing_plan_line_make_sale(orm.TransientModel):
+    _inherit = "analytic.billing.plan.line.make.sale"
+
+    def make_sales_orders(self, cr, uid, ids, context=None):
+        """
+             To make sales.
+
+             @param self: The object pointer.
+             @param cr: A database cursor
+             @param uid: ID of the user currently logged in
+             @param ids: the ID or list of IDs
+             @param context: A standard dictionary
+
+             @return: A dictionary which of fields with values.
+
+        """
+
+        if context is None:
+            context = {}
+        record_ids = context and context.get('active_ids', False)
+        res = []
+        if record_ids:            
+            billing_plan_obj = self.pool.get('analytic.billing.plan.line')
+            order_obj = self.pool.get('sale.order')
+            order_line_obj = self.pool.get('sale.order.line')
+            partner_obj = self.pool.get('res.partner')                               
+            acc_pos_obj = self.pool.get('account.fiscal.position')
+            project_obj = self.pool.get('project.project')
+                   
+            list_line = []
+
+            customer_data = False
+            company_id = False
+            sale_id = False
+            account_id = False
+
+            for line in billing_plan_obj.browse(cr, uid, record_ids, context=context):
+                                                        
+                    uom_id = line.product_uom_id
+                    
+                    if not line.customer_id:
+                        raise osv.except_osv(
+                            _('Could not create sale order !'),
+                            _('You have to enter a customer.'))   
+                    
+                    if customer_data is not False and line.customer_id != customer_data:
+                        raise osv.except_osv(
+                            _('Could not create sale order !'),
+                            _('You have to select lines from the same customer.'))
+                    else:
+                        customer_data = line.customer_id
+
+                    partner_addr = partner_obj.address_get(cr, uid, [customer_data.id],
+                                                           ['default', 'invoice', 'delivery', 'contact'])
+                    newdate = datetime.today()
+                    partner = customer_data
+                    pricelist_id = partner.property_product_pricelist and partner.property_product_pricelist.id or False
+                    price_unit = line.price_unit
+
+                    line_company_id = line.company_id and line.company_id.id or False
+                    if company_id is not False and line_company_id != company_id:
+                        raise osv.except_osv(
+                            _('Could not create sale order !'),
+                            _('You have to select lines from the same company.'))
+                    else:
+                        company_id = line_company_id        
+
+                    shop = self.pool.get('sale.shop').search(cr, uid, [('company_id', '=', company_id)])
+                    shop_id = shop and shop[0] or False
+
+                    sale_order_line = {
+                        'name': line.name,
+                        'product_uom_qty': line.unit_amount,
+                        'product_id': line.product_id.id,
+                        'product_uom': uom_id.id,
+                        'price_unit': price_unit,
+                        'notes': line.notes,
+                        'analytic_account_id': line.account_id and line.account_id.id or False,
+                    }
+
+                    taxes = False
+                    if line.product_id:
+                        taxes_ids = line.product_id.product_tmpl_id.taxes_id
+                        taxes = acc_pos_obj.map_tax(cr, uid, partner.property_account_position, taxes_ids)
+                    
+                    if taxes:
+                        sale_order_line.update({
+                            'tax_id': [(6, 0, taxes)]
+                        })
+                    list_line.append(sale_order_line)
+                    
+                    if sale_id is False:
+                        sale_id = order_obj.create(cr, uid, {
+                            'origin': '',
+                            'shop_id': shop_id,
+                            'partner_id': customer_data.id,
+                            'pricelist_id': pricelist_id,
+                            'partner_invoice_id': partner_addr['invoice'],
+                            'partner_order_id': partner_addr['contact'],
+                            'partner_shipping_id': partner_addr['delivery'],
+                            'date_order': newdate.strftime('%Y-%m-%d %H:%M:%S'),
+                            'fiscal_position': partner.property_account_position and
+                                               partner.property_account_position.id or False,
+                            'company_id': company_id,                            
+                            'payment_term': partner.property_payment_term and
+                                            partner.property_payment_term.id or False,
+                        }, context=context)
+                                                        
+                    sale_order_line.update({
+                        'order_id': sale_id
+                    })
+                    
+                    order_line_id = order_line_obj.create(cr, uid, sale_order_line, context=context)
+
+                    values = {
+                        'order_line_ids': [(4, order_line_id)]
+                    }
+
+                    billing_plan_obj.write(cr, uid, [line.id], values, context=context)
+
+                    res.append(order_line_id)
+
+        return {
+            'domain': "[('id','in', ["+','.join(map(str, res))+"])]",
+            'name': _('Billing request lines'),
+            'view_type': 'form',
+            'view_mode': 'tree,form',
+            'res_model': 'sale.order.line',
+            'view_id': False,
+            'context': False,
+            'type': 'ir.actions.act_window'
+        }
+
+analytic_billing_plan_line_make_sale()
