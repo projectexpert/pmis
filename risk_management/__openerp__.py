85c7b06d93335e317d2c21af5902617b4734ea27
Matjaž Mozetič <info@matmoz.si>
2015-08-04 21:04:26 +0200

    Migration to 8.0

--- risk_management/__openerp__.py
+++ risk_management/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Enterprise Management Solution
 #    risk_management Module
 #    Copyright (C) 2011 ValueDecision Ltd (<http://www.valuedecision.com>).
@@ -16,7 +16,7 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 {
@@ -31,36 +31,40 @@
        1) Project Management
 
        2) Business Continuity Planning
-       
-       A risk is always associated with a project and has an impact and probability assessment pre- and post-response with resulting expected values (scores).
-       Actions on risk are tasks, which can be identified easily in the extended task view (Projects > Tasks) via the 'Action on Risk' filter and associated risk id.
-       In addition, tasks which are actions on risks are coloured green in the task list (tree) view.
-       
-       Risk categories, response categories and proximity are set-up according to the PRINCE2 project methodology as well as for Business Continuity Planning.
-       They are easily changed via the 'Configuration' menu to get them in accordance with PIP, SCRUM or DSDM.
-       
+
+       A risk is always associated with a project and has an impact and probability assessment pre- and post-response
+       with resulting expected values (scores). Actions on risk are tasks, which can be identified easily in the
+       extended task view (Projects > Tasks) via the 'Action on Risk' filter and associated risk id. In addition,
+       tasks which are actions on risks are coloured green in the task list (tree) view.
+
+       Risk categories, response categories and proximity are set-up according to the PRINCE2 project methodology as
+       well as for Business Continuity Planning. They are easily changed via the 'Configuration' menu to get them in
+       accordance with PIP, SCRUM or DSDM.
+
        Besides the above mentioned information the following information can and ought to be documented as well:
        Risk description, author, date registered, date modified, event, effect, cause and risk owner.
-       
-       Two user groups are added, which a user must be associated with to work with Risks, either Risk / User or Risk / Manager.
 
-       
+       Two user groups are added, which a user must be associated with to work with Risks, either
+       Risk / User or Risk / Manager.
+
+
     """,
     'depends': ['project'],
-    'update_xml':['risk_management_data.xml',
-                  'risk_management_sequence.xml',
-                  'security/risk_management_security.xml',
-                  'security/ir.model.access.csv',
-                  'view/project_task_view.xml',
-                  'view/risk_management_view.xml',
-                  'view/risk_management_category_view.xml',
-                  'view/risk_management_category_response_view.xml',
-                  'view/risk_management_proximity_view.xml',
-                  'view/risk_management_menus.xml'],
+    'data': [
+        'risk_management_data.xml',
+        'risk_management_sequence.xml',
+        # 'security/risk_management_security.xml',
+        'security/ir.model.access.csv',
+        'view/project_task_view.xml',
+        'view/risk_management_view.xml',
+        'view/risk_management_category_view.xml',
+        'view/risk_management_category_response_view.xml',
+        'view/risk_management_proximity_view.xml',
+        'view/risk_management_menus.xml'
+    ],
     'demo': ['risk_management_demo.xml'],
     'test': ['test/test_risk_management.yml'],
     'installable': True,
     'application': True,
     'active': False,
 }
-
