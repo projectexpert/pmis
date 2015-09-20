9408e194089e769284c182746c94448821a90fc6
Matjaž Mozetič <info@matmoz.si>
2015-09-20 09:16:04 +0200

    Solve the issue duplication when creating issues from task form without immediately saving the task

--- project_task_issues/__openerp__.py
+++ project_task_issues/__openerp__.py
@@ -28,7 +28,7 @@
 ##############################################################################
 {
     'name': 'Project Task Issues',
-    'version': '0.1.3',
+    'version': '0.1.4',
     'category': 'Projects Management',
     'summary': """Creatable issues list on task view form""",
     'author': 'Matmoz d.o.o.',
