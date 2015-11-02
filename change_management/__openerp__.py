ca5b753c5e79f3a153d85eb45c67973b3a1329af
Matjaž Mozetič <info@matmoz.si>
2015-11-02 12:55:05 +0100

    Enable back to draft for CR, fix wrong onchange function on easy scheduling

--- change_management/__openerp__.py
+++ change_management/__openerp__.py
@@ -21,7 +21,7 @@
 ##############################################################################
 {
     'name': 'Change Management',
-    'version': '8.0.2.5.1',
+    'version': '8.0.2.5.2',
     'author':   'Matmoz d.o.o., '
                 'Project Expert Team',
     'contributors': [
--- project_time_easy_scheduling/__openerp__.py
+++ project_time_easy_scheduling/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Project Easy Scheduling tool',
-    'version': '8.0.1.0.0',
+    'version': '8.0.1.0.1',
     'author':   'Eficent, '
                 'Project Expert Team',
     'contributors': [
