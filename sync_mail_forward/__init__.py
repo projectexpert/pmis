1106c28bdb9f4b097fc448d2167e263557e09d28
Matjaž Mozetič <info@matmoz.si>
2015-11-14 06:52:08 +0100

    Cleaning

--- sync_mail_forward/__init__.py
+++ sync_mail_forward/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2015-Today Synconics Technologies Private Ltd.
 #
@@ -15,10 +15,8 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
 import mail_forward
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
