dc9bd1f514f90089b1dda20fe13b84f803b9b72e
Matjaž Mozetič <info@matmoz.si>
2016-11-04 07:07:39 +0100

    Kanban appearance

--- project_wbs/__openerp__.py
+++ project_wbs/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Work Breakdown Structure',
-    'version': '8.0.2.2.6',
+    'version': '8.0.2.2.7',
     'author': 'Eficent, '
               'Matmoz d.o.o., '
               'Project Expert Team',
