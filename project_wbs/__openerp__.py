fbccef0a1cde26a73668458407fd5f034d2e1120
Matjaz Mozetic <sysadmin@matmoz.si>
2017-07-01 09:47:43 +0200

    Better project kanban GUI navigation

--- project_wbs/__openerp__.py
+++ project_wbs/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Work Breakdown Structure',
-    'version': '8.0.2.3.9',
+    'version': '8.0.2.4.0',
     'author': 'Eficent, '
               'Matmoz, '
               'Luxim, '
