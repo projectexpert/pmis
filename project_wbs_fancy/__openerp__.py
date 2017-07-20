8fa08d3bab1db6070528805706fecf3bccb082ce
Matjaž Mozetič <info@matmoz.si>
2017-07-20 15:38:59 +0200

    [FIX] wrongly set xml dependencies

--- project_wbs_fancy/__openerp__.py
+++ project_wbs_fancy/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Fancy Project WBS',
-    'version': '8.0.1.0.6',
+    'version': '8.0.1.0.7',
     'summary': 'Enhance the Project WBS UI',
     'author':   'Matmoz d.o.o., '
                 'Project Expert Team',
@@ -11,7 +11,11 @@
     'website': 'http://project.expert',
     'category': 'Project Management',
     'license': 'AGPL-3',
-    'depends': ['web_tree_dynamic_colored_field', 'project_wbs'],
+    'depends': [
+        'web_tree_dynamic_colored_field',
+        'project_wbs',
+        'analytic_plan'
+    ],
     'data': [
         'views/project_view.xml',
         'views/analytic_view.xml'
