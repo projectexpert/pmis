8ce35d74855f51810c898f37064feab64f3d68f5
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-08-18 18:12:38 +0530

    [IMP] Copyright

--- stock_analytic_account/model/stock_picking.py
+++ stock_analytic_account/model/stock_picking.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2014-17 Eficent Business and IT Consulting Services S.L.
-# © 2016 Matmoz d.o.o.
+# Copyright 2014-17 Eficent Business and IT Consulting Services S.L.
+# Copyright 2016 Matmoz d.o.o.
 # Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import fields, models
