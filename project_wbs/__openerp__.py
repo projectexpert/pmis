9e574f84813eb601c16196a45c26b50d26524a28
Matjaz Mozetic <sysadmin@matmoz.si>
2017-09-27 10:49:31 +0200

    Return default_state context on project view xml that's gone missing in last commit

--- project_wbs/__openerp__.py
+++ project_wbs/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Work Breakdown Structure',
-    'version': '8.0.2.4.3',
+    'version': '8.0.2.4.4',
     'author': 'Eficent, '
               'Matmoz, '
               'Luxim, '
