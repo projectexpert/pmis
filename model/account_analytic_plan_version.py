a11227f083f7d056ff51cfa9f3607d0cb5c06b98
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-11-23 17:32:47 +0530

    [ADD] Added a unit test case & Fixed Travis

--- model/account_analytic_plan_version.py
+++ model/account_analytic_plan_version.py
@@ -1,6 +1,6 @@
- # -*- coding: utf-8 -*-
- # Copyright 2017 Eficent Business and IT Consulting Services S.L.
- # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo.tools.translate import _
 from odoo import api, fields, models
