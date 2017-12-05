6ca3f7c49319f3d89b63ba34906d7eab5f055d01
Matjaž Mozetič <m.mozetic@matmoz.si>
2017-12-05 17:43:40 +0100

    Move obsolete module

--- risk_management/model/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2011-2015 ValueDecision Ltd <http://www.valuedecision.com>.
-# Copyright (C) 2015 Neova Health <http://www.neovahealth.co.uk>.
-# Copyright (C) 2015 Matmoz d.o.o. <http://www.matmoz.si>.
-# Copyright (C) 2017 Luxim d.o.o. <http://www.luxim.si>.
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
-import risk_management
-import project_task
