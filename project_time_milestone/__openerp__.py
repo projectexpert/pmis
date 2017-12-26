cb5e7a4249033ecfc16273e8da4a997eba354c6e
Matjaž Mozetič <m.mozetic@matmoz.si>
2017-12-26 17:02:28 +0100

    Change milestone xpath to follow new task form design

--- project_time_milestone/__openerp__.py
+++ project_time_milestone/__openerp__.py
@@ -5,7 +5,7 @@
 #    License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Project Time Management - Milestones',
-    'version': '8.0.1.0.5',
+    'version': '8.0.1.0.6',
     'author':   'Eficent, '
                 'Matmoz d.o.o., '
                 'Luxim d.o.o., '
