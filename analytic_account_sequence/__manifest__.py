5edafc9be88ad340b16a4c0b75ec80c93e94cde6
aheficent <ahenriquez@eficent.com>
2018-03-28 17:26:43 +0200

    [MIG]analytic_account_sequence to v10

--- analytic_account_sequence/__manifest__.py
+++ analytic_account_sequence/__manifest__.py
@@ -1,35 +1,28 @@
 # -*- coding: utf-8 -*-
-# © 2015 Eficent Business and IT Consulting Services S.L. -
-# Jordi Ballester Alomar
-# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 Luxim d.o.o.
+# Copyright 2017 Matmoz d.o.o.
+# Copyright 2017 Deneroteam.
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Analytic account code sequence',
     'summary': 'Analytic account code sequence',
-    'version': '8.0.1.0.2',
+    'version': '10.0.1.0.0',
     'author':   'Eficent, '
                 'SerpentCS ,'
-                'Project Expert Team',
-    'contributors': [
-        'Jordi Ballester <jordi.ballester@eficent.com>',
-        'Matjaž Mozetič <m.mozetic@matmoz.si>',
-        'Sudhir Arya <http://www.serpentcs.com/>'
-    ],
-    'website': 'http://project.expert',
+                'Project Expert Team ,'
+                'Odoo Community Association (OCA)',
+    'website': 'https://www.github.com/OCA/project',
     'category': 'Project Management',
-    'depends': ['project_wbs'],
+    'depends': ['project_wbs', 'stock_analytic_account'],
     'data': [
         'views/analytic_account_sequence_view.xml',
         'data/analytic_account_sequence_data.xml',
         'views/account_analytic_account_view.xml',
         'security/ir.model.access.csv',
     ],
-    'test': [
-    ],
-    'installable': False,
-    'active': False,
-    'certificate': '',
-    'application': True,
+    'installable': True,
     'license': 'AGPL-3',
 }
