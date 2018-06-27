7765f14d7f52335ead71f18666c117cfd688c8ac
aaron <ahenriquez@eficent.com>
2018-06-27 13:47:32 +0200

    [FIX]wrong quant constraint for parts delivered from products

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,4 @@
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from . import stock_location
-from . import stock_quant
 from . import account_analytic_account
