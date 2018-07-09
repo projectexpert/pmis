6823844d6b93154773c6ca83cd4e702b3a9e4a22
aheficent <ahenriquez@eficent.com>
2018-07-09 15:48:22 +0200

    [FIX]analytic_journal licence

--- __manifest__.py
+++ __manifest__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 # Copyright 2015 Odoo SA
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Analytic Journal",
     "version": "9.0.1.0.0",
     "summary": "Analytic Journals as in previous Odoo versions",
     "author": "Odoo SA",
+    "license": "LGPL-3",
     "category": "Analytic",
     "depends": ["account", "analytic"],
     "data": ['views/analytic_view.xml',
