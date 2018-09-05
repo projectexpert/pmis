6391df201bce6bfdb7c078b030858f13a836afcc
aheficent <ahenriquez@eficent.com>
2018-09-05 10:28:32 +0200

    [DEL]picking_type_id in AA is on analytic_location module

--- models/__init__.py
+++ models/__init__.py
@@ -2,4 +2,3 @@
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from . import stock_location
-from . import account_analytic_account
