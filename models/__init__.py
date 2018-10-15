d77feff4a9ac52d9e76717668d22582a17bd7dcc
Matjaz Mozetic <info@matmoz.si>
2018-10-15 11:48:51 +0200

    Move menus in same group, move default analytic plan journals to analytic plan

--- models/__init__.py
+++ models/__init__.py
@@ -9,3 +9,4 @@ from . import account_analytic_plan_version
 from . import account_analytic_plan
 from . import account_analytic_plan_journal
 from . import account_analytic_account
+from . import product
